e701e9c481d680a2b896a02723e4a6f7e5e48b19
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2024-01-17 08:12:04 -0500

    Sync 20240117

--- data_processing/ddh5_Plotting/individual_plots/amplifiers_Ryan/NIST_2022/Sivak_Fig.py
+++ data_processing/ddh5_Plotting/individual_plots/amplifiers_Ryan/NIST_2022/Sivak_Fig.py
@@ -7,14 +7,15 @@ from cycler import cycler
 import matplotlib.pyplot as plt
 mpl.style.use('hatlab')
 
-CSV = r'C:/Users/Ryan/OneDrive - University of Pittsburgh/slides_figures/NISTAmp_2022/Sivak_Fig_vals_no_KTWPA.csv'
-df = pd.read_csv(CSV)
-fig, ax = pplt.subplots(width = '14in')
+CSV = r'E:\Ryan_Files\OneDrive - University of Pittsburgh\slides_figures\NISTAmp_2022\raw_plots/Sivak_Fig_vals_no_KTWPA.csv'
+df = pd.read_csv(CSV, error_bad_lines = 'warn')
+fig, ax = pplt.subplots(refwidth = '3in')
 mpl_markers = ['.', 'o','v','^','<','>', '1', '2', '3', '4', '8', 's', 'p', '*', 'h', '+', 'D']
 prop_cycler = cycler(marker = mpl_markers)
 ax.set_prop_cycle(prop_cycler)
 # ax.set_aspect(0.5)
-fontsize = 25
+fontsize = 10
+markersize = 30
 
 line50 = lambda x: x-3
 line001 = lambda x: x-30
@@ -25,16 +26,16 @@ for key, device in df.T.items():
         color = 'red'
     else: 
         color = 'black'
-    ax.scatter(device['Pump Power (dBm)'], device['Output Saturation Power (dBm)'], color = device['Color'], markersize = 150, label = str(device['Device'])+', '+str(device['Author'])+'('+str(device['Year'])+')')
+    ax.scatter(device['Pump Power (dBm)'], device['Output Saturation Power (dBm)'], color = device['Color'], markersize = markersize, label = str(device['Device'])+', '+str(device['Author'])+'('+str(device['Year'])+')')
     #plot the pla device
     # ax.scatter(-19.8, -52, label = 'KIPA: PHYSICAL REVIEW APPLIED 17, 034064 (2022)')
 xvals = np.linspace(-100, -10, 100)
-ax.plot(xvals, line50(xvals), 'k-', label = '100% pump utilization')
-ax.plot(xvals, line001(xvals), '--', label = '0.1% pump utilization')
+ax.plot(xvals, line50(xvals), 'k-', label = '100% PAE')
+ax.plot(xvals, line001(xvals), 'k--', label = '0.1% pump utilization')
 # ax.plot(xvals, line0001(xvals), '--', label = '0.01% efficiency')
 ax.legend(loc = 'right', ncols = 1, frameon = 0)
 ax.set_xlabel('Pump Power (dBm)', fontsize = fontsize)
 ax.set_ylabel('Output Saturation Power (dBm)', fontsize = fontsize)
 ax.tick_params(axis='both', which='major', labelsize=fontsize)
 
-fig.save(r'C:\Users\Ryan\OneDrive - University of Pittsburgh\slides_figures\pumpEfficiencyFig_noKTWPA.svg')
+# fig.save(r'C:\Users\Ryan\OneDrive - University of Pittsburgh\slides_figures\pumpEfficiencyFig_noKTWPA.svg')
